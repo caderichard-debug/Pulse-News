6d8024a433f6d4e6fcd4a0894a7d1f18d5b4b9fd
cadeberkeley <cade.richard@berkeley.edu>
2025-10-05 02:54:58 -0700

    Fixed User name showing up next to logout button.

--- backend/app/routes/auth.py
+++ backend/app/routes/auth.py
@@ -158,6 +158,7 @@ def register(
         "user": {
             "id": user.id,
             "email": user.email,
+            "name": user.name,
             "email_verified": user.email_verified
         }
     }
@@ -214,6 +215,7 @@ def login(
         "user": {
             "id": user.id,
             "email": user.email,
+            "name": user.name,
             "email_verified": user.email_verified
         }
     }
@@ -230,7 +232,8 @@ def get_current_user_info(
     """
     return {
         "id": current_user.id,
-        "email": current_user.email,  # Removed 'name' - User model doesn't have it
+        "email": current_user.email,
+        "name": current_user.name,
         "email_verified": current_user.email_verified,
         "created_at": current_user.created_at,
         "last_login": current_user.last_login
