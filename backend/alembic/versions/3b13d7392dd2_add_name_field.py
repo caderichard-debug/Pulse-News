a1f3b46f696237c10f2dc1cfcef8e746c783893f
cadeberkeley <cade.richard@berkeley.edu>
2025-10-09 13:14:48 -0500

    Updated alembic migration chane to include initial migration that creates all base tables.

--- backend/alembic/versions/3b13d7392dd2_add_name_field.py
+++ backend/alembic/versions/3b13d7392dd2_add_name_field.py
@@ -13,7 +13,7 @@ from sqlalchemy.dialects import postgresql
 
 # revision identifiers, used by Alembic.
 revision: str = '3b13d7392dd2'
-down_revision: Union[str, None] = None
+down_revision: Union[str, None] = '0001'
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
