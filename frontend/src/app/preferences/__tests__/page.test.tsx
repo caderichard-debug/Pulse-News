937db33a5ae8467cd90f4d0695edda253c274f2b
cadeberkeley <cade.richard@berkeley.edu>
2025-10-13 20:49:19 -0400

    Fixed frontend unit tests.

--- frontend/src/app/preferences/__tests__/page.test.tsx
+++ frontend/src/app/preferences/__tests__/page.test.tsx
@@ -284,6 +284,6 @@ describe('PreferencesPage', () => {
     await user.click(logoutButton);
 
     expect(api.clearToken).toHaveBeenCalled();
-    expect(mockPush).toHaveBeenCalledWith('/feed');
+    expect(mockPush).toHaveBeenCalledWith('/');
   });
 });
